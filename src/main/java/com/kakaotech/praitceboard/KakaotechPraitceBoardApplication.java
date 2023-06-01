d994fba23b04b23b323cb1cb856a9057d30ecc34
yuseonkim <yusun715@icloud.com>
2023-06-02 03:12:07 +0900

    #3 스프링부트 프로젝트 시작
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/kakaotech/praitceboard/KakaotechPraitceBoardApplication.java
@@ -0,0 +1,13 @@
+package com.kakaotech.praitceboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaotechPraitceBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaotechPraitceBoardApplication.class, args);
+    }
+
+}
