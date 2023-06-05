16c6a1a7d4369534e118e1be2e922fe349afa2f8
yuseonkim <yusun715@icloud.com>
2023-06-06 02:25:18 +0900

    #8 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1테이블 매칭되도록 설계
    생성자와 equals(), hashcode() 등이 도메인 맥락에 맞게끔 디테일하게 설정

--- src/main/java/com/kakaotech/praitceboard/domain/Article.java
+++ src/main/java/com/kakaotech/praitceboard/domain/Article.java
@@ -1,15 +1,87 @@
 package com.kakaotech.praitceboard.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString
+@Table(indexes = { //검색할 것을 넣어준다.
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+
+@Entity //테이블과 매핑할 클래스에 해당 어노테이션
 public class Article {
+
+    protected Article() {
+    }
+
+    public Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id); //
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  //AutoIncrement를 위해서
     private Long id;
+
+
+    @Setter
+    @Column(nullable = false)
     private String title;  // 제목
-    private String content; // 본문
+    @Setter
+    @Column(nullable = false)
+    private String content;// 본문
+
+    @Setter
     private String hashtag; // 해시태그
 
+
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL ) //article 테이블로부터 온것이다 명시
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
-    private String createBy;
+    @CreatedBy
+    @Column(nullable = false, length = 100)
+    private String createdBy;
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
-    private String modifedeBy;
+    @LastModifiedBy
+    private String modifiedBy; //자동세팅 - jpaAuditing
 }
