c7ed121fa0f6c325633a029f0c9679503851c211
yuseonkim <yusun715@icloud.com>
2023-06-09 15:14:20 +0900

    #1 - Spring Date REST 테스트 제외처리
    프로젝트의 비즈니스 로직으로 구현한 내용이 아니라, data rest 기능이고, 통합테스트가 무거우며 db에 영향을 주기 때문에 삭제하지는 않고 제외처리를 한다.

--- src/test/java/com/kakaotech/praitceboard/controller/DataRestTest.java
+++ src/test/java/com/kakaotech/praitceboard/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.kakaotech.praitceboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled("Spring Date REST 통합테스트는 제외 시작")
 @DisplayName("Data REST - API 테스트")
 @Transactional
 @AutoConfigureMockMvc
